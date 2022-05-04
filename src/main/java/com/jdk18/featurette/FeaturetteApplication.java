fb9a24f4caf8ade503c58b397c26a22875ee9f47
Kavish Rizvi <rizvikavish205.com>
2022-05-04 12:44:20 +0530

    initial commit

---
+++ src/main/java/com/jdk18/featurette/FeaturetteApplication.java
@@ -0,0 +1,13 @@
+package com.jdk18.featurette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaturetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaturetteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdk18/featurette/FeaturetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdk18.featurette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeaturetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
